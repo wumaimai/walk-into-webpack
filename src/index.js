f3d9e2c54f9d7e372527bf9f5f7b5bbbc9b60c7f
mai <754301684@qq.com>
2020-12-11 11:09:25 +0800

    开发

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import _ from 'lodash'
+import printMe from './print'
 import './style.css'
-import Icon from './wepay.png'
-import Data from './data.xml'
 
 function component () {
   var element = document.createElement('div')
+  var btn = document.createElement('button');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-  element.classList.add('hello')
-
-  var myIcon = new Image()
-  myIcon.src = Icon;
-  element.appendChild(myIcon)
-
-  console.log(Data);
+  btn.innerHTML = 'Click me and check the console!'
+  btn.onclick = printMe
+  element.appendChild(btn)
   return element
 }
 
-document.body.appendChild(component())
+let element = component();
+
+document.body.appendChild(element)
+
+if (module.hot) {
+  module.hot.accept('./print.js', function () {
+    console.log('Accepting the updated printMe module!');
+    //printMe()
+    document.body.removeChild(element);
+    element = component();
+    document.body.appendChild(element);
+  })
+}
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const webpack = require('webpack')
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    //print: './src/print.js'
+  },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: [
-          'csv-loader'
-        ]
-      },
-      {
-        test: /\.xml$/,
-        use: [
-          'xml-loader'
-        ]
+        use: ['style-loader', 'css-loader']
       }
     ]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 }

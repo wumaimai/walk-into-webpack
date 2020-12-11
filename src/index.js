d8b5848df1dcf2a9d16775d00148662f00a44c82
mai <754301684@qq.com>
2020-12-11 16:45:50 +0800

    懒加载

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-function getComponent () {
-  return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
-    var element = document.createElement('div')
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-    return element
-  }).catch(err => {
-    console.log('An error occurred while loading the component');
+import _ from 'lodash'
+
+function component () {
+  var element = document.createElement('div')
+  var button = document.createElement('button')
+  var br = document.createElement('br')
+
+  button.innerHTML = 'Click me and look at the console!'
+  element.appendChild(br)
+  element.appendChild(button)
+
+  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    var print = module.default
+    print()
   })
+
+  return element
 }
 
-getComponent().then(component => {
-  document.body.appendChild(component)
-})
+document.body.appendChild(component())
 
 
 
